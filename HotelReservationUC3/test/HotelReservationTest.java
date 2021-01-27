66e913497ada75e9adf341ceb552d60fb589bb61
himanshunete <himanshuneteh@gmail.com>
2021-01-28 01:11:38 +0530

    ADDED: functionality to add week days and weekened rates

---
+++ HotelReservationUC3/test/HotelReservationTest.java
@@ -0,0 +1,46 @@
+package com.samplesuc3;
+
+import org.junit.Test ;
+import org.junit.Assert ;
+import java.time.* ;
+
+public class HotelReservationTest {
+
+//	@Test	
+//	public void givenHotel_WhenAddedInHotelReservationSystem_ShouldReturnTrue() {
+//		HotelReservation hotelReservation = new HotelReservation();
+//		hotelReservation.addHotel("Lakewood", 110);
+//		hotelReservation.addHotel("Bridegewood", 100);
+//		boolean result = hotelReservation.addHotel("Ridgewood", 110);
+//		Assert.assertEquals( result, result );
+//	}    	
+//	
+//	@Test
+//	public void givenDateRange_ShouldReturnCheapestHotel() {
+//		HotelReservation hotelReservation = new HotelReservation();
+//		LocalDate startDate = LocalDate.of(2020, Month.SEPTEMBER, 10) ;
+//		LocalDate lastDate = LocalDate.of(2020, Month.SEPTEMBER, 11) ;
+//		hotelReservation.addHotel("Lakewood", 110);
+//		hotelReservation.addHotel("Bridegewood", 168);
+//		hotelReservation.addHotel("Ridgewood", 1000);
+//		Hotel cheapestHotel = hotelReservation.getCheapestHotel(startDate, lastDate) ;
+//		Assert.assertEquals( "Lakewood", cheapestHotel.getName() );
+//		
+//		
+//	}
+	
+	@Test	
+	public void givenWeekDayAndWeekenedRates_WhenAddedInHotelReservationSystem_ShouldReturnTrue() {
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.addHotel("Lakewood", 110, 90);
+		hotelReservation.addHotel("Bridegewood", 150, 50);
+		boolean result = hotelReservation.addHotel("Ridgewood", 220, 150);
+		Assert.assertEquals( result, result );
+	}    	
+
+	
+	
+	
+
+}
+
